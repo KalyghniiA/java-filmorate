28ac066fb15b108e5c2177f2a5e1e4004d4d06d2
Александр <kalyghnii@gmail.com>
2024-01-31 16:56:36 +0500

    fix

--- src/main/java/ru/yandex/practicum/filmorate/util/Util.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/Util.java
@@ -4,6 +4,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Util {
-    public final static LocalDate VALIDATION_DATE = LocalDate.of(1895, 12, 28);
-    public final static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    public static final  LocalDate VALIDATION_DATE = LocalDate.of(1895, 12, 28);
+    public static final  DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 }
