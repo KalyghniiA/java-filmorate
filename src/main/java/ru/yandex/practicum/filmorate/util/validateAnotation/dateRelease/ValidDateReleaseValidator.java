0a6a59a1347005a775d72912b435d5edb16cd731
Александр <kalyghnii@gmail.com>
2024-03-21 15:05:43 +0500

    fix servise and storage

--- src/main/java/ru/yandex/practicum/filmorate/util/validateAnotation/dateRelease/ValidDateReleaseValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/validateAnotation/dateRelease/ValidDateReleaseValidator.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 
 public class ValidDateReleaseValidator implements ConstraintValidator<ValidDateRelease, LocalDate> {
     private static final LocalDate VALIDATION_DATE = LocalDate.of(1895, 12, 28);
+
     @Override
     public boolean isValid(LocalDate date, ConstraintValidatorContext context) {
         if (date == null)
